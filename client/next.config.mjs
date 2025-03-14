34a396ea891b101da37081023639a00d4a86d860
Anuragp22 <anuragp2003b@gmail.com>
2025-03-15 03:03:54 +0530

    update image source URLs to new S3 bucket

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -3,10 +3,10 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "s3-inventorymanagement.s3.us-east-2.amazonaws.com",
-        port: "",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 's3-inventorymanagement231112233.s3.us-east-2.amazonaws.com',
+        port: '',
+        pathname: '/**',
       },
     ],
   },
